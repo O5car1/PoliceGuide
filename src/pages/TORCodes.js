c935e9ad75d6f67a49538c8b35c327db85554210
Oscar Dixon <oscaaar.11.12@gmail.com>
2024-12-08 23:44:43 +0000

    Changed: TOR Code Page

--- src/pages/TORCodes.js
+++ src/pages/TORCodes.js
@@ -2,11 +2,8 @@ import { Box, Typography } from '@mui/material';
 
 const TORCodes = () => {
     return (
-        <Box>
-            <Typography variant="h4" padding={'1%'}>TOR Codes</Typography>
-            <Box sx={{ width: '100%', height: '100vh' }}>
-                <iframe width='100%' height='100%' fill src="https://offencecode.uk/categories/traffic-offence-codes/"/>
-            </Box>
+        <Box sx={{ width: '100%', height: '100vh' }}>
+            <iframe width='100%' height='100%' fill src="https://offencecode.uk/categories/traffic-offence-codes/"/>
         </Box>
     );
 }
