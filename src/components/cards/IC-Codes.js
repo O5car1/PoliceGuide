4d474bc97cca2750697ff2983cb66eaeeed7bd79
Oscar Dixon <oscaaar.11.12@gmail.com>
2024-12-09 22:54:56 +0000

    Removed: IC Codes Colours

--- src/components/cards/IC-Codes.js
+++ src/components/cards/IC-Codes.js
@@ -9,56 +9,36 @@ function ICCodes() {
                     <Table size='small'>
                         <TableHead>
                             <TableRow>
-                                    <TableCell><Typography color='primary'>Colour</Typography></TableCell>
                                     <TableCell><Typography color='primary'>IC Code</Typography></TableCell>
                                     <TableCell><Typography color='primary'>Ethnicity</Typography></TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
                             <TableRow>
-                                <TableCell>
-                                    <Box sx={{ p: 1.25, bgcolor: 'white' }} />
-                                </TableCell>
                                 <TableCell>IC-1</TableCell>
                                 <TableCell>White (North European)</TableCell>
                             </TableRow>
                             <TableRow>
-                                <TableCell>
-                                    <Box sx={{ p: 1.25, bgcolor: 'white' }} />
-                                </TableCell>
                                 <TableCell>IC-2</TableCell>
                                 <TableCell>White (South European)</TableCell>
                             </TableRow>
                             <TableRow>
-                                <TableCell>
-                                    <Box sx={{ p: 1.25, bgcolor: 'black' }} />
-                                </TableCell>
                                 <TableCell>IC-3</TableCell>
                                 <TableCell>Black</TableCell>
                             </TableRow>
                             <TableRow>
-                                <TableCell>
-                                    <Box sx={{ p: 1.25, bgcolor: 'yellow' }} />
-                                </TableCell>
                                 <TableCell>IC-4</TableCell>
                                 <TableCell>Asian (Indian Subcontinent)</TableCell>
                             </TableRow>
                             <TableRow>
-                                <TableCell>
-                                    <Box sx={{ p: 1.25, bgcolor: 'yellow' }} />
-                                </TableCell>
                                 <TableCell>IC-5</TableCell>
                                 <TableCell>Chinese, Japanese, Korean or other Southeast Asian</TableCell>
                             </TableRow>
                             <TableRow>
-                                <TableCell>
-                                    <Box sx={{ p: 1.25, bgcolor: 'yellow' }} />
-                                </TableCell>
                                 <TableCell>IC-6</TableCell>
                                 <TableCell>Arab or North African</TableCell>
                             </TableRow>
                             <TableRow>
-                                <TableCell>Unknown</TableCell>
                                 <TableCell>IC-9</TableCell>
                                 <TableCell>Unknown</TableCell>
                             </TableRow>
