745dee30a895cce8889a7f06bc6f4f2e1b9a134d
Oscar Dixon <oscaaar.11.12@gmail.com>
2024-12-10 00:20:06 +0000

    Changed: TORCodes Page

--- src/pages/D13.js
+++ src/pages/D13.js
@@ -1,12 +1,11 @@
 import { Box, Typography } from '@mui/material';
-import D13TrainingPDF from '../content/D13Training.pdf'
 
 const D13Medical = () => {
     return (
         <Box>
             <Typography variant="h4" padding={'1%'}>D13 Medical</Typography>
-            <Box sx={{ width: '100%', height: '83vh' }}>
-                <iframe width='100%' height='100%' href="D13TrainingPDF"/>
+            <Box sx={{ width: '100%', height: '92vh' }}>
+                <object width='100%' height='100%' src="../content/D13Training.pdf"/>
             </Box>
         </Box>
     );
--- src/pages/TORCodes.js
+++ src/pages/TORCodes.js
@@ -3,7 +3,7 @@ import { Box, Typography } from '@mui/material';
 const TORCodes = () => {
     return (
         <Box sx={{ width: '100%', height: '92vh' }}>
-            <iframe width='100%' height='100%' fill src="https://offencecode.uk/categories/traffic-offence-codes/"/>
+            <iframe width='100%' height='100%' src="https://offencecode.uk/categories/traffic-offence-codes/"/>
         </Box>
     );
 }
