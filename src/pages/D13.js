b0fde2aedc18bd452530fbce5b84ecb6982637b4
Oscar Dixon <oscaaar.11.12@gmail.com>
2024-12-11 23:23:16 +0000

    Changed: D13 Training Document link

--- src/pages/D13.js
+++ src/pages/D13.js
@@ -4,8 +4,8 @@ const D13Medical = () => {
     return (
         <Box>
             <Typography variant="h4" padding={'1%'}>D13 Medical</Typography>
-            <Box sx={{ width: '100%', height: '92vh' }}>
-                <object width='100%' height='100%' src="../content/D13Training.pdf"/>
+            <Box sx={{ width: '100%', height: '83.3vh' }}>
+            <iframe width='100%' height='100%' src="https://oscar-dixon.github.io/PoliceGuide/content/D13Training.pdf"/>
             </Box>
         </Box>
     );
