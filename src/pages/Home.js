0e989998a812c9bc1f0df52b85644abdd2d26739
Oscar <32924889+O5car1@users.noreply.github.com>
2025-01-28 19:14:38 +0000

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Grid2 } from "@mui/material";
+import { Typography, Grid } from "@mui/material";
 import Necessities from "../components/cards/Necessities";
 import ICCodes from "../components/cards/IC-Codes";
 import IPP from "../components/cards/Initial-Phase-Pursuit";
@@ -9,28 +9,28 @@ import Notes from "../components/cards/Notes";
 
 const Home = () => {
   return (
-    <Grid2 container spacing={2} padding={1.5}>
-      {/* LEFT HALF */}
-      <Grid2 item xs={6}>
+    <Grid container spacing={2} padding={2}>
+      {/* LEFT COLUMN: Existing “crib sheet” content */}
+      <Grid item xs={12} md={6}>
         <Typography color="primary" variant="h4" sx={{ mb: 2 }}>
           Crib Sheet
         </Typography>
 
-        {/* Your existing 4-column layout inside this half */}
-        <Grid2 container spacing={1}>
+        {/* You can keep your original 4-column logic here */}
+        <Grid container spacing={2}>
           {/* First Column */}
-          <Grid2
+          <Grid
             item
             xs={12}
-            md={3} // or just xs={12} for smaller columns
+            md={3}
             sx={{ display: "flex", flexDirection: "column", gap: 1 }}
           >
             <ICCodes />
             <DrugClasses />
-          </Grid2>
+          </Grid>
 
           {/* Second Column */}
-          <Grid2
+          <Grid
             item
             xs={12}
             md={3}
@@ -38,39 +38,39 @@ const Home = () => {
           >
             <Necessities />
             <IPP />
-          </Grid2>
+          </Grid>
 
           {/* Third Column */}
-          <Grid2
+          <Grid
             item
             xs={12}
             md={3}
             sx={{ display: "flex", flexDirection: "column", gap: 1 }}
           >
             <GOWISELY />
-          </Grid2>
+          </Grid>
 
           {/* Fourth Column */}
-          <Grid2
+          <Grid
             item
             xs={12}
             md={3}
             sx={{ display: "flex", flexDirection: "column", gap: 2 }}
           >
             <Notes />
-          </Grid2>
-        </Grid2>
-      </Grid2>
+          </Grid>
+        </Grid>
+      </Grid>
 
-      {/* RIGHT HALF - Iframe */}
-      <Grid2
+      {/* RIGHT COLUMN: Iframe to another site */}
+      <Grid
         item
-        xs={6}
+        xs={12}
+        md={6}
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center", // optional
-          justifyContent: "flex-start", // optional
         }}
       >
         <iframe
@@ -82,8 +82,8 @@ const Home = () => {
             border: "1px solid #ccc",
           }}
         />
-      </Grid2>
-    </Grid2>
+      </Grid>
+    </Grid>
   );
 };
 
